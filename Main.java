dbafcaa9c6b7f5f29cec52e447ff4ff40e439fd0
Marina <petrukmn@yandex.ru>
2020-05-12 14:15:30 +0300

    it works!

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 package kompjuterIya.lesson040520;
 
 public class Main {
+
+    static Candy[] candyAll = new Candy[5];
+
     public static void main(String[] args) {
 
-        Candy[] candyAll = new Candy[5];
         candyAll[0] = new Candy("ChocolateBar", "Chocolate");
         candyAll[1] = new Candy("Lollipop", "Gum");
         candyAll[2] = new Candy("WrappedCandy", "Alcohol");
@@ -11,20 +13,35 @@ public class Main {
         candyAll[4] = new Candy("WrappedCandy", "Cream");
 
 
-        CandySet setForMan = new CandySetForMen("Square", 25, 1, 2);
+        CandySet setForMan = new CandySetForMen("Square", 25);
         setForMan.setCandyDone(new Candy[setForMan.setSize]);
         setForMan.congratulation();
-        setForMan.fillCandySet(candyAll[0]);
+        for (int j = 0; j < setForMan.setSize; j++) {
+            for (int i = 0; i < candyAll.length; i++) {
+                setForMan.fillCandySet(candyAll[i]);
+            }
+        }
+        setForMan.showInfo();
 
-        CandySet setForWoman = new CandySetForWomen("Heart", 25, 1, 2);
+        CandySet setForWoman = new CandySetForWomen("Heart", 25);
         setForWoman.setCandyDone(new Candy[setForWoman.setSize]);
         setForWoman.congratulation();
-        setForWoman.fillCandySet(candyAll[1]);
+        for (int j = 0; j < setForWoman.setSize; j++) {
+            for (int i = 0; i < candyAll.length; i++) {
+                setForWoman.fillCandySet(candyAll[i]);
+            }
+        }
+        setForWoman.showInfo();
 
-        CandySet setForKid = new CandySetForChildren("House", 20, 1, 0);
+        CandySet setForKid = new CandySetForChildren("House", 20);
         setForKid.setCandyDone(new Candy[setForKid.setSize]);
         setForKid.congratulation();
-        setForKid.fillCandySet(candyAll[3]);
+        for (int j = 0; j < setForKid.setSize; j++) {
+            for (int i = 0; i < candyAll.length; i++) {
+                setForKid.fillCandySet(candyAll[i]);
+            }
+        }
+        setForKid.showInfo();
     }
 }
 
